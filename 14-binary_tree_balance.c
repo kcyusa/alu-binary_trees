f7ffc1c149f4f64f2f1696c79c497223426c4bbb
cyusakeny <gasananike@gmail.com>
2025-05-15 16:48:56 +0200

    fix task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,12 +10,12 @@
  */
 static int tree_height(const binary_tree_t *tree)
 {
+int left_height, right_height;
 if (tree == NULL)
 {
 return (0);
 }
 
-int left_height, right_height;
 left_height = tree_height(tree->left);
 right_height = tree_height(tree->right);
 
